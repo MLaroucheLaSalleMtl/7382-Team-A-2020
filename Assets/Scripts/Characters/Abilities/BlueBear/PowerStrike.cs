e402ff9a6fc49b3298630878c671ac2c00ffceb5
Something222 <47398776+Something222@users.noreply.github.com>
2020-03-25 18:05:01 -0400

    Corruption fix hopefully

---
+++ Assets/Scripts/Characters/Abilities/BlueBear/PowerStrike.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerStrike : Ability
+{
+
+    public PowerStrike()
+    {
+        Id = 8;
+        Name = "Power Strike";
+        CastRange = 1;
+        Aoe = 0;
+    }
+    public override void CastAbility(Tile tileTooCastOn, int attack)
+    {
+        //High Damage Range Attack
+        Bears Target = tileTooCastOn.GetComponentInChildren<Bears>();
+        if(Target.Invincible!=false)
+        Target.Hp -= (int)(attack * 1.75);
+    }
+
+    public override string GetAbilityDesc(int attack)
+    {
+        int damage = (int)(attack * 1.75);
+        return "It's stronger: \nDamage = " + damage;
+    }
+}
